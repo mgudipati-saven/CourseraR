571408f3491e5d7d595184d1eaabfb0ea08f0376
Murthy Gudipati <mgudipati@saventech.com>
2014-01-17 13:05:52 +0530

    coursera R programming work.

---
+++ prog2/getmonitor.R
@@ -0,0 +1,17 @@
+getmonitor <- function(id, directory, summarize = FALSE) {
+  ## 'id' is a vector of length 1 indicating the monitor ID
+  ## number. The user can specify 'id' as either an integer, a
+  ## character, or a numeric.
+  
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the CSV files
+  
+  ## 'summarize' is a logical indicating whether a summary of
+  ## the data should be printed to the console; the default is
+  ## FALSE
+
+  file <- paste(directory, .Platform$file.sep, sprintf("%03d", as.numeric(id)), ".csv", sep = "")
+  data <- read.csv(file)
+  if (summarize == TRUE) print(summary(data))
+  return(data)
+}
